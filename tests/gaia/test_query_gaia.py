7080149f2e308c94cd4860ad39ce8106538343b6
astrobatty <astrobatty@gmail.com>
2023-02-24 14:12:07 +0100

    run gaia test with Marshall06 map

--- tests/gaia/test_query_gaia.py
+++ tests/gaia/test_query_gaia.py
@@ -6,20 +6,65 @@ from seismolab.gaia import query_gaia
 
 @pytest.fixture
 def targets():
-    return [1022096392749670016,3107142829863485952]
+    return [4267549637851481344,2033008274099620480]
 
 @pytest.fixture
 def results_dr3():
     results = np.genfromtxt('result_table_dr3.txt')[1:]
     return results
 
+@pytest.fixture
+def results_dr3_photodistances():
+    results = np.genfromtxt('result_table_dr3_photodist.txt')[1:]
+    return results
+
+@pytest.fixture
+def results_dr2():
+    results = np.genfromtxt('result_table_dr2.txt')[1:]
+    return results
+
+@pytest.fixture
+def results_dr2_offset():
+    results = np.genfromtxt('result_table_dr2_Stassun.txt')[1:]
+    return results
+
 def test_query_gaia_dr3(targets,results_dr3):
     targets = targets
 
-    result_table = query_gaia(targets)
+    result_table = query_gaia(targets,dustmodel='Marshall06')
     result_table = result_table.to_pandas().values.astype(float)
 
     results_in = results_dr3
+
+    assert_array_almost_equal(result_table,results_in)
+
+def test_query_gaia_dr3_photodistances(targets,results_dr3_photodistances):
+    targets = targets
+
+    result_table = query_gaia(targets,use_photodist=True,dustmodel='Marshall06')
+    result_table = result_table.to_pandas().values.astype(float)
+
+    results_in = results_dr3_photodistances
+
+    assert_array_almost_equal(result_table,results_in)
+
+def test_query_gaia_dr2(targets,results_dr2):
+    targets = targets
+
+    result_table = query_gaia(targets,gaiaDR=2,dustmodel='Marshall06')
+    result_table = result_table.to_pandas().values.astype(float)
+
+    results_in = results_dr2
+
+    assert_array_almost_equal(result_table,results_in)
+
+def test_query_gaia_dr2_offset(targets,results_dr2_offset):
+    targets = targets
+
+    result_table = query_gaia(targets,gaiaDR=2,plx_offset='Stassun',dustmodel='Marshall06')
+    result_table = result_table.to_pandas().values.astype(float)
+
+    results_in = results_dr2_offset
 
     assert_array_almost_equal(result_table,results_in)
 
